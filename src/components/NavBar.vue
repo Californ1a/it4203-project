fb596cdf57956b3832c6967532a5da765f941f5d
Californ1a <rk_california@yahoo.com>
2021-12-01 07:54:35 -0500

    navbar tweaks

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,24 @@
 <template>
-  <nav>
-    <div class="nav-wrapper blue darken-3">
-      <a href="#!" class="brand-logo">IT4203 Project</a>
-      <form>
-        <div class="input-field blue darken-1">
-          <input id="search" type="search" required>
-          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-          <i class="material-icons">close</i>
-        </div>
-      </form>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="sass.html">
-            <i class="material-icons left">search</i>Link with Left Icon</a></li>
-        <li><a href="badges.html">
-            <i class="material-icons right">view_module</i>Link with Right Icon</a></li>
-      </ul>
-    </div>
-  </nav>
+  <div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper blue darken-3">
+        <a href="#!" class="brand-logo">IT4203 Project</a>
+        <form>
+          <div class="input-field blue darken-1">
+            <input id="search" type="search" required>
+            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+            <i class="material-icons">close</i>
+          </div>
+        </form>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="sass.html">
+              <i class="material-icons left">search</i>Link with Left Icon</a></li>
+          <li><a href="badges.html">
+              <i class="material-icons right">view_module</i>Link with Right Icon</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -28,13 +30,13 @@ export default {
 </script>
 
 <style scoped>
-  nav {
+  /* nav {
     position: sticky;
     top: 0;
     left: 0;
     right: 0;
     z-index: 100;
-  }
+  } */
 
   nav .nav-wrapper {
     display: flex;
@@ -48,11 +50,17 @@ export default {
 
   .nav-wrapper .input-field input[type=search] {
     width: calc(100vw - 50rem);
+    min-width: 10rem;
   }
 
   nav form {
     line-height: 50px;
     height: 80%;
+  }
+
+  nav form i,
+  nav form i.material-icons {
+    line-height: 50px;
   }
 
   @media only screen and (min-width: 601px) {
