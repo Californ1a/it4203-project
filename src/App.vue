b88e386b3dd9e3102bdc9db1b4c893654fe2c42d
Californ1a <rk_california@yahoo.com>
2021-12-04 02:15:57 -0500

    custom scrollbar

--- src/App.vue
+++ src/App.vue
@@ -17,4 +17,25 @@ export default {
 body {
   background-color: #1B1725;
 }
+
+::-webkit-scrollbar {
+  width: 0.5em;
+}
+
+::-webkit-scrollbar:horizontal {
+  height: 0.6em;
+}
+
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
+  background: #666;
+}
 </style>
